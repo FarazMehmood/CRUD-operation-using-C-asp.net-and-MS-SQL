44b85652cfee2e1536e91fdcdedc6f43a2473182
Malik Faraz Mehmood <88116474+FarazMehmoood@users.noreply.github.com>
2022-06-17 13:21:02 -0700

    Add files via upload

---
+++ quiz1/Default.aspx
@@ -0,0 +1,136 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style2 {
+            width: 381px;
+        }
+        .auto-style3 {
+            width: 72%;
+        }
+        .auto-style4 {
+            width: 94%;
+            margin-right: 258px;
+            height: 166px;
+        }
+        .auto-style5 {
+            width: 100%;
+        }
+        .auto-style6 {
+            margin-left: 0px;
+            margin-top: 0px;
+        }
+        .auto-style7 {
+            width: 451px;
+        }
+        .auto-style8 {
+            width: 149px;
+        }
+        .auto-style10 {
+            width: 1368px;
+        }
+        .auto-style11 {
+            width: 305px;
+        }
+        .auto-style12 {
+            width: 76%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table border="1" class="auto-style3">
+                <tr>
+                    <td class="auto-style10">
+                        <table class="auto-style5">
+                            <tr>
+                                <td class="auto-style7">
+                                    <asp:Label ID="LblError" runat="server" Font-Italic="True" ForeColor="#FF3300" Text="Label Error"></asp:Label>
+                                </td>
+                                <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style7">CRUD OPERATION</td>
+                                <td>ALL AVAILABLE BOOKS</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <table class="auto-style3">
+                <tr>
+                    <td class="auto-style2">
+                        <table class="auto-style4">
+                            <tr>
+                                <td class="auto-style8">Book ID:</td>
+                                <td>
+                                    <asp:TextBox ID="TextBox1" runat="server" Width="140px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style8">Book Name:</td>
+                                <td>
+                                    <asp:TextBox ID="TextBox2" runat="server" Width="140px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style8">Book Category:</td>
+                                <td>
+                                    <asp:TextBox ID="TextBox3" runat="server" Width="140px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style8">Edition:</td>
+                                <td>
+                                    <asp:TextBox ID="TextBox4" runat="server" Width="140px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style8">Publisher:</td>
+                                <td>
+                                    <asp:TextBox ID="TextBox5" runat="server" Width="140px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style8">Print Date:</td>
+                                <td>
+                                    <asp:TextBox ID="TextBox6" runat="server" Width="140px" OnTextChanged="TextBox6_TextChanged"></asp:TextBox>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td>
+                        <asp:GridView ID="GridView1" runat="server" BorderStyle="Dotted" CssClass="auto-style6" Width="496px" BorderColor="#999966" BackColor="White">
+                        </asp:GridView>
+                    </td>
+                </tr>
+            </table>
+            <table class="auto-style12">
+                <tr>
+                    <td>
+                        <asp:Button ID="btnSave" runat="server" BorderColor="#9966FF" BorderStyle="Double" Font-Bold="True" Font-Size="Medium" OnClick="Button1_Click" Text="Save" Width="139px" />
+                    </td>
+                    <td>
+                        <asp:Button ID="btnUdt" runat="server" BorderColor="#9966FF" BorderStyle="Double" Font-Bold="True" Font-Size="Medium" OnClick="Update_Click" Text="Update" Width="139px" />
+                    </td>
+                    <td>
+                        <asp:Button ID="btnDel" runat="server" BorderColor="#9966FF" BorderStyle="Double" Font-Bold="True" Font-Size="Medium" OnClick="Delete_Click" Text="Delete" Width="139px" />
+                    </td>
+                    <td>
+                        <asp:Button ID="btnSrh" runat="server" BorderColor="#9966FF" BorderStyle="Double" Font-Bold="True" Font-Size="Medium" OnClick="Search_Click" Text="Search" Width="139px" />
+                    </td>
+                    <td class="auto-style11">
+                        <asp:Button ID="btnShow" runat="server" BorderColor="#9966FF" BorderStyle="Double" Font-Bold="True" Font-Size="Medium" OnClick="ALL_Click" Text="Show All" Width="139px" />
+                    &nbsp;<asp:Button ID="btnReset" runat="server" BorderColor="#9966FF" BorderStyle="Double" Font-Bold="True" Font-Size="Medium" OnClick="Reset_Click" Text="Reset" Width="139px" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ quiz1/Default.aspx.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+
+    }
+
+
+    private void pointer()
+    {
+
+        string connString = System.Configuration.ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString;
+        try
+        {
+            SqlConnection objConn = new SqlConnection(connString);
+            if (objConn.State == ConnectionState.Closed)
+            {
+                objConn.Open();
+                string comandString = "select * from BookDetail";
+                SqlCommand cmnd = new SqlCommand(comandString, objConn);
+                DataTable dt = new DataTable();
+                
+                SqlDataAdapter da = new SqlDataAdapter(cmnd);
+                da.Fill(dt);
+              
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                objConn.Close();
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+            LblError.Text = ex.Message;
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string connString = System.Configuration.ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString;
+        try
+        {
+            SqlConnection objConn = new SqlConnection(connString);
+            if (objConn.State == ConnectionState.Closed)
+            {
+                objConn.Open();
+                string comandString = "insert into BookDetail values('"+int.Parse(TextBox1.Text)+"','"+TextBox2.Text+"','"+TextBox3.Text+"','"+TextBox4.Text+"','"+ TextBox5.Text + "','" + TextBox6.Text + "')";
+                SqlCommand cmnd = new SqlCommand(comandString, objConn);
+                cmnd.ExecuteNonQuery();
+                LblError.Text = "Data has been saved sucessfully";
+                objConn.Close();
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+            LblError.Text = ex.Message;
+        }
+    }
+    protected void Search_Click(object sender, EventArgs e)
+    {
+
+        string connString = System.Configuration.ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString;
+        try
+        {
+            SqlConnection objConn = new SqlConnection(connString);
+            if (objConn.State == ConnectionState.Closed)
+            {
+                objConn.Open();
+                string comandString = "select * from BookDetail where BookID= '" + TextBox1.Text + "'";
+                SqlCommand cmnd = new SqlCommand(comandString, objConn);
+                SqlDataReader dr = cmnd.ExecuteReader();
+                if (dr.HasRows)
+                {
+
+                    dr.Read();
+                    TextBox2.Text = dr[1].ToString();
+                    TextBox3.Text = dr[2].ToString();
+                    TextBox4.Text = dr[3].ToString();
+                    TextBox5.Text = dr[4].ToString();
+                    TextBox6.Text = dr[5].ToString();
+                    dr.Close();
+                   
+                }
+                else
+                {
+                    LblError.Text = "No data to show";
+                }
+
+                objConn.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+
+            LblError.Text = ex.Message;
+        }
+    }
+        protected void Button3_Click(object sender, EventArgs e)
+    {
+        string connString = System.Configuration.ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString;
+        try
+        {
+            SqlConnection objConn = new SqlConnection(connString);
+            if (objConn.State == ConnectionState.Closed)
+            {
+                objConn.Open();
+                LblError.Text = "Data has been saved sucessfully";
+                objConn.Close();
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+            LblError.Text = ex.Message;
+        }
+    }
+    protected void Update_Click(object sender, EventArgs e)
+    {
+        string connString = System.Configuration.ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString;
+        try
+        {
+            SqlConnection objConn = new SqlConnection(connString);
+            if (objConn.State == ConnectionState.Closed)
+            {
+                objConn.Open();
+                string comandString = "update BookDetail set  BookName = '" + TextBox2.Text + "', BookCategory = '" + TextBox3.Text + "',Edition ='" + TextBox4.Text + "',Publisher ='" + TextBox5.Text + "',PrintDate ='" + TextBox6.Text + "' where BookID = '" + TextBox1.Text + "'";
+                SqlCommand cmnd = new SqlCommand(comandString, objConn);
+                cmnd.ExecuteNonQuery();
+                LblError.Text = "Data has been updated sucessfully";
+                objConn.Close();
+             
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+            LblError.Text = ex.Message;
+        }
+    }
+
+    protected void ALL_Click(object sender, EventArgs e)
+    {
+        string connString = System.Configuration.ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString;
+        try
+        {
+            SqlConnection objConn = new SqlConnection(connString);
+            if (objConn.State == ConnectionState.Closed)
+            {
+                objConn.Open();
+                string comandString = "select * from BookDetail";
+                SqlCommand cmnd = new SqlCommand(comandString, objConn);
+                DataTable dt = new DataTable();
+                DataSet ds = new DataSet();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmnd);
+                da.Fill(dt);
+                da.Fill(ds);
+
+
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                objConn.Close();
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+            LblError.Text = ex.Message;
+        }
+    }
+    protected void Delete_Click(object sender, EventArgs e)
+    {
+        string connString = System.Configuration.ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString;
+        try
+        {
+            SqlConnection objConn = new SqlConnection(connString);
+            if (objConn.State == ConnectionState.Closed)
+            {
+                objConn.Open();
+                string comandString = "Delete from BookDetail where BookID = '" + TextBox1.Text + "'";
+                SqlCommand cmnd = new SqlCommand(comandString, objConn);
+                cmnd.ExecuteNonQuery();
+                LblError.Text = "Data has been Deleted sucessfully";
+                objConn.Close();
+                pointer();
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+            LblError.Text = ex.Message;
+        }
+    }
+    protected void Reset_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text = "";
+        TextBox2.Text = "";
+        TextBox3.Text = "";
+        TextBox4.Text = "";
+        TextBox5.Text = "";
+        TextBox6.Text = "";
+
+
+    }
+
+    protected void TextBox6_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
